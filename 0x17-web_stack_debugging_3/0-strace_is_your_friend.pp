b0feb77bd186c9930974ed9d3fd85992b3c2675a
MohammedAmer1993 <49499544+MohammedAmer1993@users.noreply.github.com>
2024-06-17 22:18:02 +0300

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/puppet
-# this file troubleshoot wrong naming in php file
-
-exec { 'move-wp-locale-file':
-  command => 'mv /var/www/html/wp-includes/class-wp-locale.phpp /var/www/html/wp-includes/class-wp-locale.php',
-  creates => '/var/www/html/wp-includes/class-wp-locale.php',
-  cwd     => '/root',
-  path    => '/bin'
-}
