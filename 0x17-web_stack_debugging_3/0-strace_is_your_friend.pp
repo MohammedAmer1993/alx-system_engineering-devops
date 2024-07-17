68a15ad472ffcfe5d50bd4186551850944ce16b2
Mohammed Amer <loard.amer.2010@gmail.com>
2024-07-17 14:25:44 +0300

    defer to c22

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/puppet
-# this file troubleshoot wrong naming in php file
-
-exec { 'move-wp-locale-file':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/bin:/usr/local/bin'
-}
