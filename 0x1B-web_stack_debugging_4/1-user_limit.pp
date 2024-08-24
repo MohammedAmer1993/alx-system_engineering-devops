5dba8baee6d044ca14776a3ec5d848cc4584f841
Mohammed Amer <loard.amer.2010@gmail.com>
2024-08-25 01:55:26 +0300

    second task of web stack debugging 4

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,12 +2,12 @@
 
 #set hard limit 
 exec { 'increase-hard-file-limit-for-holberton-user':
-    command => 'sed -i "/^holberton hard/s/4/50000/" /etc/security/limits.conf',
+    command => 'sed -i "/^holberton hard/s/5/50000/" /etc/security/limits.conf',
     path    => '/usr/local/bin/:/bin/'
 }
 
 #set soft limit
 exec { 'increase-soft-file-limit-for-holberton-user':
-    command => 'sed -i "/^holberton soft/s/5/50000/" /etc/security/limits.conf',
+    command => 'sed -i "/^holberton soft/s/4/50000/" /etc/security/limits.conf',
     path    => '/usr/local/bin/:/bin/'
 }
