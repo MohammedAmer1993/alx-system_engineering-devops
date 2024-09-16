d0e2865ad7abb4dd770579d9afe2bb972f945839
Mohammed Amer <loard.amer.2010@gmail.com>
2024-09-16 19:39:27 +0300

    all

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,10 +10,10 @@ def number_of_subscribers(subreddit):
     Get the number of subscribers for a given subreddit
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'user-agent': 'requests/2.32.3'}
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code != 200:
+    if response.status_code == 404:
         return 0
 
     data = response.json().get("data")
